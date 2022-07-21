ec17cbf088186e3a5fe5ca7bb979394517a1b0f1
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-07-21 12:48:38 -0400

    Update custom_mod_extended.py

--- custom_mod_extended/models/custom_mod_extended.py
+++ custom_mod_extended/models/custom_mod_extended.py
@@ -17,10 +17,10 @@ class CustomModExtended(models.Model):
     cap_customer_favorite_genre = fields.Char(string='Favorite Genre')
 
 
-    def CalculateAge(cap_customer_birth_year, cap_customer_age):
-        cap_customer_age = datetime.year - cap_customer_birth_year
+    #def CalculateAge(cap_customer_birth_year, cap_customer_age):
+     #   cap_customer_age = datetime.year - cap_customer_birth_year
 
-        return cap_customer_age
+      #  return cap_customer_age
 
     
     
