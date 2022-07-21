9447875b108f5994417eb7f652ff784a0aebe397
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-07-21 15:10:51 -0400

    Update custom_mod_extended.py

--- custom_mod_extended/models/custom_mod_extended.py
+++ custom_mod_extended/models/custom_mod_extended.py
@@ -16,10 +16,10 @@ class CustomModExtended(models.Model):
     cap_customer_age = fields.Integer(string='Customer Age')
     cap_customer_address = fields.Char(string='Customer Address')
     cap_customer_favorite_genre = fields.Char(string='Favorite Genre')
-    cap_year = 2022
+    
     @api.onchange('cap_customer_birth_year')
     def calculate_age(cap_customer_birth_year, cap_year):
-        
+        cap_year = 2022
         cap_customer_age = cap_year - cap_customer_birth_year
 
         return cap_customer_age
