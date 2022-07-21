ff338b8dd3717ce1f9f387397950c527e0c4990d
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-07-21 12:46:43 -0400

    Update custom_mod_extended.py

--- custom_mod_extended/models/custom_mod_extended.py
+++ custom_mod_extended/models/custom_mod_extended.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from odoo import api, models, fields
 import logging
 _logger = logging.getLogger(__name__)
@@ -14,6 +15,13 @@ class CustomModExtended(models.Model):
     cap_customer_age = fields.Integer(string='Customer Age')
     cap_customer_address = fields.Char(string='Customer Address')
     cap_customer_favorite_genre = fields.Char(string='Favorite Genre')
+
+
+    def CalculateAge(cap_customer_birth_year, cap_customer_age):
+        cap_customer_age = datetime.year - cap_customer_birth_year
+
+        return cap_customer_age
+
     
     
     
