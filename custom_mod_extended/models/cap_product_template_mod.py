4c845fc00313650bfaeacee981337871d0782bcf
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-07-24 19:43:48 -0400

    Update cap_product_template_mod.py

--- custom_mod_extended/models/cap_product_template_mod.py
+++ custom_mod_extended/models/cap_product_template_mod.py
@@ -5,7 +5,7 @@ from odoo import api, models, fields
 import logging
 _logger = logging.getLogger(__name__)
 
-class CustomModExtended(models.Model):
+class CustomModExtended1(models.Model):
 
     _inherit = 'product.template'
     
