ae7a96adf189b5b97246d9f136949bafeeee568f
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-07-25 11:24:02 -0400

    yes yes yall

--- contacts_mod_extended/__manifest__.py
+++ contacts_mod_extended/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Cap_Products',
-    'category': 'Sales',
+    'category': 'Products',
     'summary': "Overrides functionality of display name on Contact Model",
     'version': '1.0 (v13)',
     'author': 'Captivea software Consulting, Bassim Elsamaloty',
@@ -10,9 +10,9 @@
     'license': 'OPL-1',
     'description': """Overrides the display name of contacts for delivery addresses
         """,
-    'depends': ['base', 'product'],
+    'depends': ['base', 'products'],
     'data': [
-        'views/contacts_view.xml',
+        'views/products_view.xml',
     ],
     'installable': True,
     'auto_install': False,
