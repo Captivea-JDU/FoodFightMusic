b0a2bb605a6caded11f00c70ab44c691fe9a36c3
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-08-06 14:38:10 -0400

    Update __manifest__.py

--- custom_mod_extended/__manifest__.py
+++ custom_mod_extended/__manifest__.py
@@ -3,12 +3,12 @@
 {
     'name': 'Custom Module',
     'category': 'Sales',
-    'summary': "Pulls phone number associated with sale order",
+    'summary': "Fuck these lame ahh mfs man, do you",
     'version': '1.0 (v13)',
     'author': 'Captivea software Consulting, Jarvis Dumas',
     'website': 'https://www.captivea.com/',
     'license': 'OPL-1',
-    'description': """Pulls phone number associated with sale order
+    'description': """Fuck these lame ahh mfs man, do you
         """,
     'depends': ['base', 'sale'],
     'data': [
