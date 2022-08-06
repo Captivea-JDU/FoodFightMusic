6ed783c51449c79071011b7c92760fe064653612
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-08-06 13:41:14 -0400

    Update __manifest__.py

--- custom_mod_extended/__manifest__.py
+++ custom_mod_extended/__manifest__.py
@@ -13,7 +13,6 @@
     'depends': ['base', 'sale'],
     'data': [
         'views/views.xml',
-        'views/cap_product_template.xml',
     ],
     'installable': True,
     'auto_install': False,
